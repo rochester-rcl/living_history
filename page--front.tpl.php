093c4fc65212acc7bb6048706b9657168151e8bb
Sean Morris <smorris@library.rochester.edu>
2015-12-16 17:43:21 -0500

    Finished header starting on cover image portion

--- page--front.tpl.php
+++ page--front.tpl.php
@@ -1,13 +1,22 @@
 
 
-
+<!--====== UNIVERSITY HEADER ======-->
 <div class="uofr-header">
-  <a href="#">
-    <img class="navbar-brand-uofr-logo" alt="University of Rochester" src="<?php print base_path() . drupal_get_path('theme', 'living_history');?>/images/logo-uofr.png" />
-    Testing
-  </a>
+  <div class="container">
+    <!-- UofR Container -->
+    <div class="uofr-logo-container">
+      <a href="#">
+        <img class="navbar-brand-uofr-logo" alt="University of Rochester" src="<?php print base_path() . drupal_get_path('theme', 'living_history');?>/images/logo-uofr.png" />
+      </a>
+    </div>
+    <!-- Ever Better Container -->
+    <div class="everbetter-container">
+        <img class="navbar-brand-everbetter" alt="University of Rochester" src="<?php print base_path() . drupal_get_path('theme', 'living_history');?>/images/everbetter.png" />
+    </div>
+  </div>
 </div>
 
-<div class="uofr-header">
-
+<!-- Cover Image -->
+<div class="cover-container">
+    <img class="cover" alt="University of Rochester" src="<?php print base_path() . drupal_get_path('theme', 'living_history');?>/images/cover.jpg" />
 </div>
