cf5f61cf51a18be3b3d6498172a02550cebb0967
Sean Morris <smorris@library.rochester.edu>
2015-12-16 16:48:47 -0500

    Starting the files

---
+++ page--front.tpl.php
@@ -0,0 +1,13 @@
+
+
+
+<div class="uofr-header">
+  <a href="#">
+    <img class="navbar-brand-uofr-logo" alt="University of Rochester" src="<?php print base_path() . drupal_get_path('theme', 'living_history');?>/images/logo-uofr.png" />
+    Testing
+  </a>
+</div>
+
+<div class="uofr-header">
+
+</div>
