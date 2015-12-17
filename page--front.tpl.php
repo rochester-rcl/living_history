c700ee859d9d971313bf4c7da0c03adc3dc23ef5
Sean Morris <smorris@library.rochester.edu>
2015-12-17 18:18:09 -0500

    Main boxes in place

--- page--front.tpl.php
+++ page--front.tpl.php
@@ -57,34 +57,89 @@
     <img class="cover" alt="University of Rochester" src="<?php print base_path() . drupal_get_path('theme', 'living_history');?>/images/cover.jpg" />
     <div class="container">
       <div class="tagline">
-        Oral history interviews intended to preserve University of Rochester history have been conducted since at least the 1960s. The various divisions of the University--the College, the Medical Center, the Eastman School of Music, the Memorial Art Gallery--have each created recordings.
+        The oral history interviews that preserve the memories of our alumni, faculty, and staff from 1904 to the present are the foundation of the University of Rochester’s Living History Project. Recordings of other events – some filled with pomp and circumstance, others noisy and spontaneous – also preserve University history as it was lived, adding to our understanding of time, place, and memory.
       </div>
     </div>
 </div>
 
 
 <!-- 4 Boxes  -->
-<div class="box">
-  <img class="box-images" alt="Living HistoryProject" src="<?php print base_path() . drupal_get_path('theme', 'living_history');?>/images/performances.jpg" />
+<div class="box box-odd">
+  <img class="box-images" alt="Living HistoryProject" src="<?php print base_path() . drupal_get_path('theme', 'living_history');?>/images/lhp.jpg" />
+  <div class="box-text">
+    <div class="box-headline">Living History Project</div>
+    <div class="box-tagline">Oral History Interviews</div>
+  </div>
 </div>
 
-<div class="box">
-  <img class="box-images" alt="Performances" src="<?php print base_path() . drupal_get_path('theme', 'living_history');?>/images/performances.jpg" />
+<div class="box box-even">
+  <img class="box-images" alt="Performances" src="<?php print base_path() . drupal_get_path('theme', 'living_history');?>/images/performances2.jpg" />
+  <div class="box-text">
+    <div class="box-headline">Performances</div>
+    <div class="box-tagline">Theater, Music</div>
+  </div>
 </div>
 
-
-<div class="box">
+<div class="box box-odd">
   <img class="box-images" alt="Events" src="<?php print base_path() . drupal_get_path('theme', 'living_history');?>/images/events.jpg" />
+  <div class="box-text">
+    <div class="box-headline">Events</div>
+    <div class="box-tagline">Dedications, Convocations</div>
+  </div>
 </div>
 
-<div class="box">
+<div class="box box-even">
   <img class="box-images" alt="Spoken Word" src="<?php print base_path() . drupal_get_path('theme', 'living_history');?>/images/spoken-word.jpg" />
+  <div class="box-text">
+    <div class="box-headline">Spoken Word</div>
+    <div class="box-tagline">Lectures, Readings</div>
+  </div>
 </div>
 <!--/-->
 
-<!--====== Footer ======-->
+<!--====== FOOTER ======-->
 <div class="footer">
-  Footer
+    <div class="container">
+      <!-- Right Side of Footer -->
+        <div class="footer-right">
+          <div class="address">
+            <p>
+              Rare Books Special Collections Preservation <br>
+              Rush Rhees Library <br>
+              Second Floor, Room 225 <br>
+              755 Library Road <br>
+              University of Rochester	<br>
+              Rochester, NY 14627-0055 <br>
+            </p>
+            <p>
+              Phone: (585) 275-4477 <br>
+              Fax: (585) 273-1032 <br>
+              <a href="mailto:rarebks@library.rochester.edu">rarebks@library.rochester.edu</a>
+            </p>
+          </div>
+        </div>
+
+      <!-- Left Side of Footer -->
+      <div class="footer-left">
+        <!-- <div class="social-icons">
+          <a href="https://www.facebook.com/rivercampuslibraries"><i class="fa fa-facebook-square"></i></a>
+          <a href="https://twitter.com/rclibraries"><i class="fa fa-twitter-square"></i></a>
+          <a href="https://www.youtube.com/user/RCLibraries"><i class="fa fa-youtube-square"></i></a>
+          <a href="https://www-flickr-com.pc181.lib.rochester.edu/photos/carlsonlibrary/"><i class="fa fa-flickr"></i></a>
+        </div> -->
+        <!-- Copyright  -->
+        <div class="copyright">Copyright © 1998-2015. All Rights Reserved.<br>
+          University of Rochester | River Campus Libraries <br>
+          Rare Books, Special Collections and Preservation
+        </div>
+        <div class="footer-links">
+          <?php print render($page['footer']); ?>
+        </div>
+      </div>
+
+
+
+
 </div>
 
 
